9f849bc2449053a99095c0ef159161d5867a23f0
Michael <mike.witk@gmail.com>
2019-05-22 15:53:58 +0000

    Add Server Data rendered on Modal List

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,8 +1,17 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Route, Redirect, Link, Switch} from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
-import List from './pages/List';
+import React, { Component } from "react";
+// import { BrowserRouter, Route, Redirect, Link, Switch } from "react-router-dom";
+import { Link, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import List from "./pages/List";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import {
+  Route,
+  NavLink,
+  HashRouter,
+  BrowserRouter
+} from "react-router-dom/cjs/react-router-dom";
 
 const Fruit = props => {
   const handleClose = () => {
@@ -53,25 +62,60 @@ const Fruits = props => {
   );
 };
 
-
-
-
 class App extends Component {
   render() {
     const App = () => (
       <div>
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/list' component={List}/>
+          <Route exact path="/" component={Home} />
+          <Route path="/list" component={List} />
           <Route path="/fruits" render={props => <Fruits {...props} />} />
-          <Redirect exact path="/" to="/fruits" />
+          {/* <Redirect exact path="/" to="/fruits" /> */}
         </Switch>
       </div>
-    )
+    );
     return (
-      <Switch>
-        <App/>
-      </Switch>
+      // <Switch>
+      //   <App />
+      // </Switch>
+      <HashRouter>
+        <div>
+          <h1>U</h1>
+          <Container className="container">
+            <Row>
+              <div>
+                <NavLink to="/list">
+                  <i className="fas fa-bars" />
+                  List
+                </NavLink>
+              </div>
+              <div>
+                <NavLink to="/fruits">
+                  <i className="fas fa-bars" />
+                  fruits
+                </NavLink>
+              </div>
+              <div>
+                <NavLink to="/dailyChallenge">
+                  <i className="fas fa-bars" />
+                  Daily Challenges
+                </NavLink>
+              </div>
+              <div>
+                <NavLink to="/todoList">
+                  <i className="fas fa-bars" />
+                  Todo List
+                </NavLink>
+              </div>
+              <div className="content">
+                <Route exact path="/" component={Home} />
+                <Route path="/list" component={List} />
+                <Route path="/fruits" render={props => <Fruits {...props} />} />
+              </div>
+            </Row>
+          </Container>
+        </div>
+      </HashRouter>
     );
   }
 }
--- client/src/App/pages/List.js
+++ client/src/App/pages/List.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class List extends Component {
   // Initialize the state
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       list: []
-    }
+    };
   }
 
   // Fetch the list on first mount
@@ -16,40 +16,34 @@ class List extends Component {
 
   // Retrieves the list of items from the Express app
   getList = () => {
-    fetch('/api/getList', {
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-      })
-    .then(res => res.json())
-    .then(list => this.setState({ list }))
-  }
+    fetch("/api/getList", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(list => this.setState({ list }));
+  };
 
   render() {
-    const { list } = this.state;
+    // const { list } = this.state;
+    // const handleClose = () => {
+    //   props.history.push(props.basePath);
+    // };
 
     return (
-      <div className="App">
-        <h1>List of Items</h1>
-        {/* Check to see if any items are found*/}
-        {list.length ? (
-          <div>
-            {/* Render the list of items */}
-            {list.map((item) => {
-              return(
-                <div>
-                  {item}
-                </div>
-              );
-            })}
+      <div className="w3-modal" style={{ display: "block" }}>
+        <div className="w3-modal-content">
+          <div className="w3-container">
+            <h1>List of Items</h1>
+            <div>
+              {this.state.list.map(item => {
+                return <div>{item}</div>;
+              })}
+            </div>
           </div>
-        ) : (
-          <div>
-            <h2>No List Items Found</h2>
-          </div>
-        )
-      }
+        </div>
       </div>
     );
   }
