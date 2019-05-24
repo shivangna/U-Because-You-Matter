c743c59723bb9a3284e9f87aa5ff7bb1034215f7
shivangna <shivangna.kaistha@gmail.com>
2019-05-24 23:04:45 +0000

    show the journal entry as an alert, does not yet post on DB

--- client/src/App/pages/Journal.js
+++ client/src/App/pages/Journal.js
@@ -4,7 +4,40 @@ import { Form, Modal, Button } from "react-bootstrap";
 import Wordgraph from "./wordgraph.js"
 
 class Journal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entries: [],
+      value:''
+    };
+  }
+
+  componentDidMount() {
+    this.getList();
+    console.log("entries:", this.state);
+  }
+
+  getList = () => {
+    fetch("/journal")
+      .then(res => res.json())
+      .then(results => this.setState({ entries: results }));
+  };
+
+
+  handleSubmit = (event) => {
+    alert('An entry was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  }
+  
+
   render() {
+
+
     return (
       <Modal show={this.props.show} onHide={this.props.onHide}>
         <div className="App">
@@ -36,14 +69,25 @@ class Journal extends Component {
                   </Form.Control>
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlTextarea1">
-                  <Form.Label>How is your day going?</Form.Label>
-                  <Form.Control as="textarea" rows="3" />
+                <form onSubmit={this.handleSubmit}>
+                <label> add your text 
+                  <input type='text' value = {this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type ='submit' value = 'Submit' />
+                </form>
                 </Form.Group>
               </Form>
             </p>
             <Wordgraph/>
+
+            {/* <Form.Group controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>How is your day going?</Form.Label>
+                  <Form.Control as="textarea" rows="3" />
+                </Form.Group> */}
+
+                
             <p>
-              <Button variant="outline-info">Submit</Button>
+              
             </p>
           </Jumbotron>
         </div>
