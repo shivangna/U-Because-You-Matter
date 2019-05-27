9a8ad637816eca6c148e31455d195690663705df
Jocelyne EL TAHAN <lyne1604@gmail.com>
2019-05-27 18:55:03 +0000

    todo

--- client/src/App/pages/TodoList.js
+++ client/src/App/pages/TodoList.js
@@ -4,22 +4,31 @@ import { ListGroup } from "react-bootstrap";
 import { Form, Modal, Button } from "react-bootstrap";
 
 class TodoList extends Component {
-  componentDidUpdate() {
-    // this.props.currentItem.focus();
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: ""
+    };
   }
-  createTasks(item) {
-    return (
-      <li key={item.key}>
-        {item.text};
-        <span>
-          <i class="fa fa-trash" />
-        </span>{" "}
-      </li>
-    );
-  }
+
+  submitHandler = e => {
+    console.log("e =", e.target.value);
+    this.setState({ task: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    fetch("/todo", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        task: this.state.task,
+        task_state: 0
+      })
+    });
+  };
+
   render() {
-    const todoEntries = this.props.listEntries;
-    const listItems = todoEntries.map(this.createTasks);
     return (
       <Modal show={this.props.show} onHide={this.props.onHide}>
         <div className="App">
@@ -27,25 +36,22 @@ class TodoList extends Component {
             <h1>To Do</h1>
             <div id="container">
               <h1>
-                To-Do List{" "}
+                To-Do List
                 <span id="toggle-form">
                   <i class="fa fa-plus" />
                 </span>
               </h1>
-              <form onSubmit={this.props.addItem}>
+              <form onSubmit={this.handleSubmit}>
                 <input
-                  ref={this.props.inputElement}
-                  value={this.props.currentItem.text}
-                  onChange={this.props.handleInput}
+                  onChange={this.submitHandler}
                   type="text"
                   placeholder="Add New Todo"
                 />
                 <button type="submit"> Add Task </button>
               </form>
-
-              <ul className="thelist">{listItems}</ul>
+              <ul className="thelist" />
             </div>
-          </Jumbotron>{" "}
+          </Jumbotron>
         </div>
         <Modal.Footer>
           <Button variant="secondary" onClick={this.props.onHide}>
