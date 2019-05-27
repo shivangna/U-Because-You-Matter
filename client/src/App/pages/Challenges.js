c49579e2914bc4af0e85d8603622120f241411b9
Michael <mike.witk@gmail.com>
2019-05-27 01:03:18 +0000

    Bug fixing percentage

--- client/src/App/pages/Challenges.js
+++ client/src/App/pages/Challenges.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Jumbotron } from "react-bootstrap";
-import { Form, Modal, Button } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
+// import { VictoryPie, VictoryAnimation, VictoryLabel } from "victory";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 class Challenges extends Component {
   constructor(props) {
@@ -11,34 +14,56 @@ class Challenges extends Component {
       color3: "white",
       color4: "white",
       color5: "white",
-      color6: "white"
+      color6: "white",
+      color7: "green",
+      count: 1,
+      value: 0
     };
   }
 
   render() {
+    const changeCount = () => {
+      let arr = Object.values(this.state);
+      // console.log("ObjArr: ", objArr);
+      var counts = {};
+
+      for (var i = 0; i < arr.length; i++) {
+        var num = arr[i];
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+      }
+      console.log("Green count: ", counts["green"]);
+      this.setState({ count: counts["green"] });
+      return this.setState({ value: (this.state.count / 6).toFixed(2) });
+    };
     const changeColor1 = () => {
-      var newColor = this.state.color1 == "white" ? "green" : "white";
+      var newColor = this.state.color1 === "white" ? "green" : "white";
       this.setState({ color1: newColor });
+      changeCount();
     };
     const changeColor2 = () => {
-      var newColor = this.state.color2 == "white" ? "green" : "white";
+      var newColor = this.state.color2 === "white" ? "green" : "white";
       this.setState({ color2: newColor });
+      changeCount();
     };
     const changeColor3 = () => {
-      var newColor = this.state.color3 == "white" ? "green" : "white";
+      var newColor = this.state.color3 === "white" ? "green" : "white";
       this.setState({ color3: newColor });
+      changeCount();
     };
     const changeColor4 = () => {
-      var newColor = this.state.color4 == "white" ? "green" : "white";
+      var newColor = this.state.color4 === "white" ? "green" : "white";
       this.setState({ color4: newColor });
+      changeCount();
     };
     const changeColor5 = () => {
-      var newColor = this.state.color5 == "white" ? "green" : "white";
+      var newColor = this.state.color5 === "white" ? "green" : "white";
       this.setState({ color5: newColor });
+      changeCount();
     };
     const changeColor6 = () => {
-      var newColor = this.state.color6 == "white" ? "green" : "white";
+      var newColor = this.state.color6 === "white" ? "green" : "white";
       this.setState({ color6: newColor });
+      changeCount();
     };
 
     return (
@@ -53,12 +78,19 @@ class Challenges extends Component {
           </Jumbotron>{" "}
         </div>
         <div className="circleOutside">
+          <CircularProgressbar
+            value={this.state.value}
+            maxValue={1}
+            text={`${this.state.value * 100}%`}
+          />
           <div className="ciclegraph">
             <div
               id="color1"
               className="circle"
               style={{ backgroundColor: this.state.color1 }}
-              onClick={changeColor1}
+              onClick={() => {
+                changeColor1();
+              }}
             >
               <p>Take 3 deeps breaths</p>
             </div>
